708bee44a7f64029f477c45fe7618ce9ef246db6
Dmytriy <dmytriy.demydov@techmagic.co>
2018-08-13 19:01:02 +0300

    Initial commit

---
+++ src/components/Konami.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
+
+class Konami extends Component {
+    constructor() {
+        super();
+        this.popUp = this.popUp.bind(this);
+    }
+
+    componentDidMount() {
+        Mousetrap.bind([`1 2 3 4`, `up up down down left right left right b a`], this.popUp);
+    }
+
+    componentWillUnmount() {
+        Mousetrap.unbind([`1 2 3 4`, `up up down down left right left right b a`]);
+    }
+
+    popUp() {
+        alert(`You inputted the Konami code${this.props.name.length > 1 ? `, ${this.props.name}!` : `!`}`)
+    }
+
+    render() {
+        return (
+            <div>Konami Code: Activated</div>
+        );
+    }
+}
+
+export default Konami;
